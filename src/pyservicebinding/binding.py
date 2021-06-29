51ebf97ba02b549b8635991f0e39f74dc9ed7803
Baiju Muthukadan <baiju.m.mail@gmail.com>
2021-06-29 22:30:08 +0530

    Add API

---
+++ src/pyservicebinding/binding.py
@@ -0,0 +1,15 @@
+import typing
+
+def all_bindings() -> list[dict[str, str]]:
+    """Get all bndings as a list of dictionaries
+
+    - return empty list if no bindings found
+    """
+
+def get_binding(_type: str, provider: typing.Optional[str] = None) -> dict[str, str]:
+    """Get bnding as a dictionary for a give type and optional provider
+
+    - return empty dictionary if no binding found
+    - raise KeyError if duplicate entry found
+    """
+
---
+++ src/pyservicebinding/binding_test.py
@@ -0,0 +1,5 @@
+def test_get_binding():
+    pass
+
+def test_all_bindings():
+    pass
