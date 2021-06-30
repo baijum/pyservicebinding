568b2ee45ca038008c2f74ff9e63b6ed35c55eba
Baiju Muthukadan <baiju.m.mail@gmail.com>
2021-06-30 09:27:57 +0530

    add more tests and change logic

--- src/pyservicebinding/binding.py
+++ src/pyservicebinding/binding.py
@@ -4,23 +4,75 @@ import typing
 class ServiceBindingRootMissingError(KeyError):
     pass
 
+class DuplicateEntryError(KeyError):
+    pass
+
 def all_bindings() -> list[dict[str, str]]:
-    """Get all bndings as a list of dictionaries
+    """Get all bindings as a list of dictionaries
 
     - return empty list if no bindings found
-    """
-
-def get_binding(_type: str, provider: typing.Optional[str] = None) -> dict[str, str]:
-    """Get bnding as a dictionary for a give type and optional provider
-
-    - return empty dictionary if no binding found
-    - raise LookupError if duplicate entry found
     - raise ServiceBindingRootMissingError if SERVICE_BINDING_ROOT env var not set
     """
 
     try:
-        os.environ["SERVICE_BINDING_ROOT"]
+        root = os.environ["SERVICE_BINDING_ROOT"]
     except KeyError as msg:
         raise ServiceBindingRootMissingError(msg)
 
-    return {}
+    l = []
+    for dirname in os.listdir(root):
+        b = {}
+        for filename in os.listdir(os.path.join(root, dirname)):
+            b[filename] = open(os.path.join(root, dirname, filename)).read().strip()
+
+        l.append(b)
+
+    return l
+
+def get_binding(_type: str, provider: typing.Optional[str] = None) -> dict[str, str]:
+    """Get binding as a dictionary for a given type and optional provider
+
+    - return empty dictionary if no binding found
+    - raise DuplicateEntryError if duplicate entry found
+    - raise ServiceBindingRootMissingError if SERVICE_BINDING_ROOT env var not set
+    """
+
+    try:
+        root = os.environ["SERVICE_BINDING_ROOT"]
+    except KeyError as msg:
+        raise ServiceBindingRootMissingError(msg)
+
+    b = {}
+    dupcheck = []
+    if provider:
+        for dirname in os.listdir(root):
+            typepath = os.path.join(root, dirname, "type")
+            providerpath = os.path.join(root, dirname, "provider")
+            if os.path.exists(typepath):
+                typevalue = open(typepath).read().strip()
+                if typevalue != _type:
+                    continue
+                if os.path.exists(providerpath):
+                    providervalue = open(providerpath).read().strip()
+                    if providervalue != provider:
+                        continue
+                    dupcheck.append(typevalue + ":" + providervalue)
+
+                    for filename in os.listdir(os.path.join(root, dirname)):
+                        b[filename] = open(os.path.join(root, dirname, filename)).read().strip()
+    else:
+        for dirname in os.listdir(root):
+            typepath = os.path.join(root, dirname, "type")
+            if os.path.exists(typepath):
+                typevalue = open(typepath).read().strip()
+                if typevalue != _type:
+                    continue
+                dupcheck.append(typevalue)
+
+                for filename in os.listdir(os.path.join(root, dirname)):
+                    b[filename] = open(os.path.join(root, dirname, filename)).read().strip()
+
+    if len(dupcheck) > 1 and all(x==dupcheck[0] for x in dupcheck):
+        raise DuplicateEntryError(dupcheck)
+
+    return b
--- src/pyservicebinding/binding_test.py
+++ src/pyservicebinding/binding_test.py
@@ -1,6 +1,3 @@
-import os
-import sys
-
 import pytest
 
 from pyservicebinding import binding
@@ -12,15 +9,34 @@ def test_get_binding(tmpdir, monkeypatch):
     _type.write("mysql")
     username = sb1.join("username")
     username.write("john")
+    password = sb1.join("password")
+    password.write("L&ia6W@n7epi18a")
+    url = sb1.join("url")
+    url.write("mysql://192.168.94.102:3306/school")
+
+    sb2 = tmpdir.join("bindings").mkdir("sb2")
+    _type = sb2.join("type")
+    _type.write("neo4j")
+    username = sb2.join("username")
+    username.write("jane")
+    password = sb2.join("password")
+    password.write("o4%bGt#D8v2i0ja")
+    uri = sb2.join("uri")
+    uri.write("neo4j://192.168.94.103:7687/cr")
 
     monkeypatch.setenv("SERVICE_BINDING_ROOT", str(bindings_dir))
 
     b = binding.get_binding("mysql")
-    #assert b["username"] == "john"
-    #b["password"] = "L&ia6W@n7epi18a"
-    #b["url"] = "mysql://192.168.94.102:3306/school"
+    assert b["username"] == "john"
+    assert b["password"] == "L&ia6W@n7epi18a"
+    assert b["url"] == "mysql://192.168.94.102:3306/school"
 
-def test_get_binding_missing_service_binding_root(tmpdir):
+    b = binding.get_binding("neo4j")
+    assert b["username"] == "jane"
+    assert b["password"] == "o4%bGt#D8v2i0ja"
+    assert b["uri"] == "neo4j://192.168.94.103:7687/cr"
+
+def test_get_binding_with_duplicate_entry_error(tmpdir, monkeypatch):
     bindings_dir = tmpdir.mkdir("bindings")
     sb1 = tmpdir.join("bindings").mkdir("sb1")
     _type = sb1.join("type")
@@ -28,13 +44,143 @@ def test_get_binding_missing_service_binding_root(tmpdir):
     username = sb1.join("username")
     username.write("john")
 
+    sb2 = tmpdir.join("bindings").mkdir("sb2")
+    _type = sb2.join("type")
+    _type.write("mysql")
+    username = sb2.join("username")
+    username.write("jane")
+
+    monkeypatch.setenv("SERVICE_BINDING_ROOT", str(bindings_dir))
+
+    with pytest.raises(binding.DuplicateEntryError):
+        binding.get_binding("mysql")
+
+def test_get_binding_with_duplicate_entry_error_different_provider(tmpdir, monkeypatch):
+    bindings_dir = tmpdir.mkdir("bindings")
+    sb1 = tmpdir.join("bindings").mkdir("sb1")
+    _type = sb1.join("type")
+    _type.write("mysql")
+    username = sb1.join("username")
+    username.write("john")
+
+    sb2 = tmpdir.join("bindings").mkdir("sb2")
+    _type = sb2.join("type")
+    _type.write("mysql")
+    provider = sb2.join("provider")
+    provider.write("mariadb")
+    username = sb2.join("username")
+    username.write("jane")
+
+    monkeypatch.setenv("SERVICE_BINDING_ROOT", str(bindings_dir))
+
+    with pytest.raises(binding.DuplicateEntryError):
+        binding.get_binding("mysql")
+
+def test_get_binding_without_duplicate_entry_error_different_provider(tmpdir, monkeypatch):
+    bindings_dir = tmpdir.mkdir("bindings")
+    sb1 = tmpdir.join("bindings").mkdir("sb1")
+    _type = sb1.join("type")
+    _type.write("mysql")
+    username = sb1.join("username")
+    username.write("john")
+
+    sb2 = tmpdir.join("bindings").mkdir("sb2")
+    _type = sb2.join("type")
+    _type.write("mysql")
+    provider = sb2.join("provider")
+    provider.write("mariadb")
+    username = sb2.join("username")
+    username.write("jane")
+
+    monkeypatch.setenv("SERVICE_BINDING_ROOT", str(bindings_dir))
+
+    b = binding.get_binding("mysql", "mariadb")
+    assert b["username"] == "jane"
+
+
+def test_get_binding_with_duplicate_entry_error_same_provider(tmpdir, monkeypatch):
+    bindings_dir = tmpdir.mkdir("bindings")
+    sb1 = tmpdir.join("bindings").mkdir("sb1")
+    _type = sb1.join("type")
+    _type.write("mysql")
+    provider = sb1.join("provider")
+    provider.write("mariadb")
+    username = sb1.join("username")
+    username.write("john")
+
+    sb2 = tmpdir.join("bindings").mkdir("sb2")
+    _type = sb2.join("type")
+    _type.write("mysql")
+    provider = sb2.join("provider")
+    provider.write("mariadb")
+    username = sb2.join("username")
+    username.write("jane")
+
+    monkeypatch.setenv("SERVICE_BINDING_ROOT", str(bindings_dir))
+
+    with pytest.raises(binding.DuplicateEntryError):
+        binding.get_binding("mysql")
+
+
+def test_get_binding_missing_service_binding_root(tmpdir):
+    sb1 = tmpdir.mkdir("bindings").mkdir("sb1")
+    _type = sb1.join("type")
+    _type.write("mysql")
+    username = sb1.join("username")
+    username.write("john")
 
     with pytest.raises(binding.ServiceBindingRootMissingError):
         binding.get_binding("mysql")
-    #assert b["username"] == "john"
-    #b["password"] = "L&ia6W@n7epi18a"
-    #b["url"] = "mysql://192.168.94.102:3306/school"
 
 
-def test_all_bindings():
-    pass
+def test_all_bindings(tmpdir, monkeypatch):
+    bindings_dir = tmpdir.mkdir("bindings")
+    sb1 = tmpdir.join("bindings").mkdir("sb1")
+    _type = sb1.join("type")
+    _type.write("mysql")
+    username = sb1.join("username")
+    username.write("john")
+    password = sb1.join("password")
+    password.write("L&ia6W@n7epi18a")
+    url = sb1.join("url")
+    url.write("mysql://192.168.94.102:3306/school")
+
+    sb2 = tmpdir.join("bindings").mkdir("sb2")
+    _type = sb2.join("type")
+    _type.write("neo4j")
+    username = sb2.join("username")
+    username.write("jane")
+    password = sb2.join("password")
+    password.write("o4%bGt#D8v2i0ja")
+    uri = sb2.join("uri")
+    uri.write("neo4j://192.168.94.103:7687/cr")
+
+    monkeypatch.setenv("SERVICE_BINDING_ROOT", str(bindings_dir))
+
+    l = binding.all_bindings()
+    assert len(l) == 2
+    count = 0
+    for b in l:
+        if b["type"] == "mysql":
+            count = count + 1
+            assert b["username"] == "john"
+            assert b["password"] == "L&ia6W@n7epi18a"
+            assert b["url"] == "mysql://192.168.94.102:3306/school"
+
+        if b["type"] == "neo4j":
+            count = count + 1
+            assert b["username"] == "jane"
+            assert b["password"] == "o4%bGt#D8v2i0ja"
+            assert b["uri"] == "neo4j://192.168.94.103:7687/cr"
+
+    assert len(l) == 2
+
+def test_all_bindings_missing_service_binding_root(tmpdir):
+    sb1 = tmpdir.mkdir("bindings").mkdir("sb1")
+    _type = sb1.join("type")
+    _type.write("mysql")
+    username = sb1.join("username")
+    username.write("john")
+
+    with pytest.raises(binding.ServiceBindingRootMissingError):
+        binding.all_bindings()
