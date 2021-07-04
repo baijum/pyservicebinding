751417eeb0e980c83410d498e1c4f45fa26c7468
Baiju Muthukadan <baiju.m.mail@gmail.com>
2021-07-04 20:24:26 +0530

    Change API to the proposed API

--- src/pyservicebinding/binding.py
+++ src/pyservicebinding/binding.py
@@ -1,78 +1,73 @@
 import os
 import typing
 
-class ServiceBindingRootMissingError(KeyError):
+class ServiceBindingRootMissingError(Exception):
     pass
 
-class DuplicateEntryError(KeyError):
-    pass
 
-def all_bindings() -> list[dict[str, str]]:
-    """Get all bindings as a list of dictionaries
+class ServiceBinding:
 
-    - return empty list if no bindings found
-    - raise ServiceBindingRootMissingError if SERVICE_BINDING_ROOT env var not set
-    """
+    def __init__(self):
+        try:
+            self.root = os.environ["SERVICE_BINDING_ROOT"]
+        except KeyError as msg:
+            raise ServiceBindingRootMissingError(msg)
 
-    try:
-        root = os.environ["SERVICE_BINDING_ROOT"]
-    except KeyError as msg:
-        raise ServiceBindingRootMissingError(msg)
 
-    l = []
-    for dirname in os.listdir(root):
-        b = {}
-        for filename in os.listdir(os.path.join(root, dirname)):
-            b[filename] = open(os.path.join(root, dirname, filename)).read().strip()
+    def all_bindings(self) -> list[dict[str, str]]:
+        """Get all bindings as a list of dictionaries
 
-        l.append(b)
-
-    return l
-
-def get_binding(_type: str, provider: typing.Optional[str] = None) -> dict[str, str]:
-    """Get binding as a dictionary for a given type and optional provider
-
-    - return empty dictionary if no binding found
-    - raise DuplicateEntryError if duplicate entry found
-    - raise ServiceBindingRootMissingError if SERVICE_BINDING_ROOT env var not set
-    """
-
-    try:
-        root = os.environ["SERVICE_BINDING_ROOT"]
-    except KeyError as msg:
-        raise ServiceBindingRootMissingError(msg)
-
-    b = {}
-    dupcheck = []
-    if provider:
+        - return empty list if no bindings found
+        """
+        root = self.root
+        l = []
         for dirname in os.listdir(root):
-            typepath = os.path.join(root, dirname, "type")
-            providerpath = os.path.join(root, dirname, "provider")
-            if os.path.exists(typepath):
-                typevalue = open(typepath).read().strip()
-                if typevalue != _type:
-                    continue
-                if os.path.exists(providerpath):
-                    providervalue = open(providerpath).read().strip()
-                    if providervalue != provider:
+            b = {}
+            for filename in os.listdir(os.path.join(root, dirname)):
+                b[filename] = open(os.path.join(root, dirname, filename)).read().strip()
+
+            l.append(b)
+
+        return l
+
+    def bindings(self, _type: str, provider: typing.Optional[str] = None) -> list[dict[str, str]]:
+        """Get filtered bindings as a list of dictionaries
+
+        - return empty dictionary if no binding found
+        - filter the result with the given _type input
+        - if provider input is given, filter bindings using the given type and provider
+        """
+        root = self.root
+        l = []
+        b = {}
+        if provider:
+            for dirname in os.listdir(root):
+                typepath = os.path.join(root, dirname, "type")
+                providerpath = os.path.join(root, dirname, "provider")
+                if os.path.exists(typepath):
+                    typevalue = open(typepath).read().strip()
+                    if typevalue != _type:
                         continue
-                    dupcheck.append(typevalue + ":" + providervalue)
+                    if os.path.exists(providerpath):
+                        providervalue = open(providerpath).read().strip()
+                        if providervalue != provider:
+                            continue
+
+                        for filename in os.listdir(os.path.join(root, dirname)):
+                            b[filename] = open(os.path.join(root, dirname, filename)).read().strip()
+
+                        l.append(b)
+        else:
+            for dirname in os.listdir(root):
+                typepath = os.path.join(root, dirname, "type")
+                if os.path.exists(typepath):
+                    typevalue = open(typepath).read().strip()
+                    if typevalue != _type:
+                        continue
 
                     for filename in os.listdir(os.path.join(root, dirname)):
                         b[filename] = open(os.path.join(root, dirname, filename)).read().strip()
-    else:
-        for dirname in os.listdir(root):
-            typepath = os.path.join(root, dirname, "type")
-            if os.path.exists(typepath):
-                typevalue = open(typepath).read().strip()
-                if typevalue != _type:
-                    continue
-                dupcheck.append(typevalue)
 
-                for filename in os.listdir(os.path.join(root, dirname)):
-                    b[filename] = open(os.path.join(root, dirname, filename)).read().strip()
+                    l.append(b)
 
-    if len(dupcheck) > 1 and all(x==dupcheck[0] for x in dupcheck):
-        raise DuplicateEntryError(dupcheck)
-
-    return b
+        return l
--- src/pyservicebinding/binding_test.py
+++ src/pyservicebinding/binding_test.py
@@ -2,7 +2,8 @@ import pytest
 
 from pyservicebinding import binding
 
-def test_get_binding(tmpdir, monkeypatch):
+
+def test_bindings(tmpdir, monkeypatch):
     bindings_dir = tmpdir.mkdir("bindings")
     sb1 = tmpdir.join("bindings").mkdir("sb1")
     _type = sb1.join("type")
@@ -26,111 +27,21 @@ def test_get_binding(tmpdir, monkeypatch):
 
     monkeypatch.setenv("SERVICE_BINDING_ROOT", str(bindings_dir))
 
-    b = binding.get_binding("mysql")
+    sb = binding.ServiceBinding()
+    l = sb.bindings("mysql")
+    b = l[0]
     assert b["username"] == "john"
     assert b["password"] == "L&ia6W@n7epi18a"
     assert b["url"] == "mysql://192.168.94.102:3306/school"
 
-    b = binding.get_binding("neo4j")
+    l = sb.bindings("neo4j")
+    b = l[0]
     assert b["username"] == "jane"
     assert b["password"] == "o4%bGt#D8v2i0ja"
     assert b["uri"] == "neo4j://192.168.94.103:7687/cr"
 
-def test_get_binding_with_duplicate_entry_error(tmpdir, monkeypatch):
-    bindings_dir = tmpdir.mkdir("bindings")
-    sb1 = tmpdir.join("bindings").mkdir("sb1")
-    _type = sb1.join("type")
-    _type.write("mysql")
-    username = sb1.join("username")
-    username.write("john")
-
-    sb2 = tmpdir.join("bindings").mkdir("sb2")
-    _type = sb2.join("type")
-    _type.write("mysql")
-    username = sb2.join("username")
-    username.write("jane")
-
-    monkeypatch.setenv("SERVICE_BINDING_ROOT", str(bindings_dir))
-
-    with pytest.raises(binding.DuplicateEntryError):
-        binding.get_binding("mysql")
-
-def test_get_binding_with_duplicate_entry_error_different_provider(tmpdir, monkeypatch):
-    bindings_dir = tmpdir.mkdir("bindings")
-    sb1 = tmpdir.join("bindings").mkdir("sb1")
-    _type = sb1.join("type")
-    _type.write("mysql")
-    username = sb1.join("username")
-    username.write("john")
-
-    sb2 = tmpdir.join("bindings").mkdir("sb2")
-    _type = sb2.join("type")
-    _type.write("mysql")
-    provider = sb2.join("provider")
-    provider.write("mariadb")
-    username = sb2.join("username")
-    username.write("jane")
-
-    monkeypatch.setenv("SERVICE_BINDING_ROOT", str(bindings_dir))
-
-    with pytest.raises(binding.DuplicateEntryError):
-        binding.get_binding("mysql")
-
-def test_get_binding_without_duplicate_entry_error_different_provider(tmpdir, monkeypatch):
-    bindings_dir = tmpdir.mkdir("bindings")
-    sb1 = tmpdir.join("bindings").mkdir("sb1")
-    _type = sb1.join("type")
-    _type.write("mysql")
-    username = sb1.join("username")
-    username.write("john")
-
-    sb2 = tmpdir.join("bindings").mkdir("sb2")
-    _type = sb2.join("type")
-    _type.write("mysql")
-    provider = sb2.join("provider")
-    provider.write("mariadb")
-    username = sb2.join("username")
-    username.write("jane")
-
-    monkeypatch.setenv("SERVICE_BINDING_ROOT", str(bindings_dir))
-
-    b = binding.get_binding("mysql", "mariadb")
-    assert b["username"] == "jane"
-
-
-def test_get_binding_with_duplicate_entry_error_same_provider(tmpdir, monkeypatch):
-    bindings_dir = tmpdir.mkdir("bindings")
-    sb1 = tmpdir.join("bindings").mkdir("sb1")
-    _type = sb1.join("type")
-    _type.write("mysql")
-    provider = sb1.join("provider")
-    provider.write("mariadb")
-    username = sb1.join("username")
-    username.write("john")
-
-    sb2 = tmpdir.join("bindings").mkdir("sb2")
-    _type = sb2.join("type")
-    _type.write("mysql")
-    provider = sb2.join("provider")
-    provider.write("mariadb")
-    username = sb2.join("username")
-    username.write("jane")
-
-    monkeypatch.setenv("SERVICE_BINDING_ROOT", str(bindings_dir))
-
-    with pytest.raises(binding.DuplicateEntryError):
-        binding.get_binding("mysql")
-
-
-def test_get_binding_missing_service_binding_root(tmpdir):
-    sb1 = tmpdir.mkdir("bindings").mkdir("sb1")
-    _type = sb1.join("type")
-    _type.write("mysql")
-    username = sb1.join("username")
-    username.write("john")
-
-    with pytest.raises(binding.ServiceBindingRootMissingError):
-        binding.get_binding("mysql")
+    l = sb.bindings("non-existing")
+    assert len(l) == 0
 
 
 def test_all_bindings(tmpdir, monkeypatch):
@@ -157,7 +68,8 @@ def test_all_bindings(tmpdir, monkeypatch):
 
     monkeypatch.setenv("SERVICE_BINDING_ROOT", str(bindings_dir))
 
-    l = binding.all_bindings()
+    sb = binding.ServiceBinding()
+    l = sb.all_bindings()
     assert len(l) == 2
     count = 0
     for b in l:
@@ -175,7 +87,8 @@ def test_all_bindings(tmpdir, monkeypatch):
 
     assert len(l) == 2
 
-def test_all_bindings_missing_service_binding_root(tmpdir):
+
+def test_missing_service_binding_root(tmpdir):
     sb1 = tmpdir.mkdir("bindings").mkdir("sb1")
     _type = sb1.join("type")
     _type.write("mysql")
@@ -183,4 +96,4 @@ def test_all_bindings_missing_service_binding_root(tmpdir):
     username.write("john")
 
     with pytest.raises(binding.ServiceBindingRootMissingError):
-        binding.all_bindings()
+        sb = binding.ServiceBinding()
